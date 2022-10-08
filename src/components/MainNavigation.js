8ca166e94cafcfe1f90887da145992024c6c7573
Darren-Choo <darren_ryu@hotmail.com>
2022-10-08 17:30:43 +0800

    -

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -4,16 +4,19 @@ import classes from "./MainNavigation.module.css";
 function MainNavigation() {
   return (
     <header className={classes.header}>
+      {/* <div></div> */}
       <nav>
         <ul>
           <Link to="/">
             <b>PriceFix</b>
           </Link>
+
           <li className="btn--black">
             <Link to="/login">
               <b>Login</b>
             </Link>
           </li>
+
           <li className="btn--white">
             <Link to="/sign-up">
               <b>Sign Up</b>
