ae1e828a9ff0c24657350876d656dc1cf590c0be
Darren-Choo <darren_ryu@hotmail.com>
2022-10-09 21:55:25 +0800

    -

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
-import LargeBold from "./texts/LargeBold";
+// import LargeBold from "./texts/LargeBold";
 
 function MainNavigation() {
   return (
