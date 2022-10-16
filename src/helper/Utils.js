8715a2fdb9a9fa3bbeeb8b12526ce8b363148055
leeeecheer <64578934+leeeecheer@users.noreply.github.com>
2022-10-16 16:07:35 +0800

    Create Utils.js

---
+++ src/helper/Utils.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+const Utils = {};
+const baseUrl = "http://localhost:8080";
+
+const onErrorHandler = (err) => {
+  // user not logged in or token given is invalid
+  if (err.response.status === 403 || err.response.status === 401) {
+    return { message: "Unauthenticated" };
+  }
+
+  // unknown error by server
+  if (err.response.status === 500) {
+    return { message: "Unknown error" };
+  }
+};
+
+Utils.postApi = async function (endPoint, parameters = {}) {
+  // Prepare the API endpoint to send request too
+  const url = baseUrl + endPoint;
+
+  try {
+    // Making an API request
+    const res = await axios({
+      method: "POST",
+      url: url,
+      // headers: {
+      // }
+      data: parameters,
+    });
+
+    return res;
+  } catch (err) {
+    return onErrorHandler(err);
+  }
+};
+
+Utils.getApi = async function (endPoint, parameters = {}) {
+  const url = baseUrl + endPoint;
+
+  try {
+    const res = await axios.get(url, {
+      params: parameters,
+    });
+
+    return res;
+  } catch (err) {
+    return onErrorHandler(err);
+  }
+};
+
+export default Utils;
