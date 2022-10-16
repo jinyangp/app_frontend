f402414ca77981d4c9eb932f8f02ae941cafbe48
leeeecheer <64578934+leeeecheer@users.noreply.github.com>
2022-10-16 15:39:39 +0800

    Update Category.js

--- src/components/Categories/Category/Category.js
+++ src/components/Categories/Category/Category.js
@@ -16,25 +16,30 @@ import useStyles from "./styles";
 const Category = ({ category }) => {
   const classes = useStyles();
   return (
-    <Card className={ClassNames.root}>
-      <CardMedia
-        className={classes.media}
-        image={category.image}
-        title={category.type}
-      />
-      <CardContent>
-        <div className={classes.CardContent}>
-          <Typography variant="h5" gutterBottom>
-            {category.type}
+    <button>
+      <Card className={ClassNames.root}>
+        <CardMedia
+          className={classes.media}
+          image={category.image}
+          title={category.type}
+        />
+        <CardContent>
+          <div className={classes.CardContent}>
+            <Typography variant="h5" gutterBottom>
+              {category.type}
+            </Typography>
+            <Typography variant="h5">{category.Amount}</Typography>
+          </div>
+          <Typography variant="body2" color="textSecondary">
+            {category.description}
           </Typography>
-          <Typography variant="h5">{category.Amount}</Typography>
-        </div>
-        <Typography variant="body2" color="textSecondary">
-          {category.description}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing className={classes.cardActions}></CardActions>
-    </Card>
+        </CardContent>
+        <CardActions
+          disableSpacing
+          className={classes.cardActions}
+        ></CardActions>
+      </Card>
+    </button>
   );
 };
 
