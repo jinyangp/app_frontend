16326ed167e9a0337fbe94e5b6c226ee7c21fa5c
leeeecheer <64578934+leeeecheer@users.noreply.github.com>
2022-10-17 12:45:25 +0800

    Update Utils.js

--- src/helper/Utils.js
+++ src/helper/Utils.js
@@ -4,6 +4,10 @@ const Utils = {};
 const baseUrl = "http://localhost:8080";
 
 const onErrorHandler = (err) => {
+  // user not logged in or token given is invalid
+  if (err.response.status === 403 || err.response.status === 401) {
+    return { message: "Unauthenticated" };
+  }
   // unknown error by server
   if (err.response.status === 500) {
     return { message: "Unknown error" };
