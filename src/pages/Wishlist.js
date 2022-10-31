9436ec6e2a751d75989066a442c457d4b94db9e0
Darren-Choo <darren_ryu@hotmail.com>
2022-10-31 17:02:55 +0800

    import issue fixed

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -1,13 +1,12 @@
 import MainNavigation from "../components/MainNavigation";
+import AddToWishlistButton from "../components/buttons/AddToWishlistButton";
 
 function Wishlist(props) {
   return (
-
     <div className="default-page-margin">
       <MainNavigation />
       <AddToWishlistButton />
     </div>
-
   );
 }
 
